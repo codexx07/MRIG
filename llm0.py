7a99b8e046547e0b76e0d59781e575b772102c12
Sparsh <sparsh.chakraborty07@gmail.com>
2023-11-05 02:52:26 +0530

    working llm feat

---
+++ llm0.py
@@ -0,0 +1,63 @@
+from langchain.vectorstores import FAISS
+from langchain.llms import GooglePalm
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+from langchain.llms import GooglePalm
+api_key=("AIzaSyDDwPxe4yKWwf-6Rq7vkpOkihlVr6uaxkA")
+llm = GooglePalm(google_api_key=api_key,temperature=0.001)
+instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
+vectordb_file_path = "faiss_index"
+
+def create_vector_db():
+    # Load data from FAQ sheet
+    loader = CSVLoader(file_path=r'C:\Users\Sarabjot Singh\OneDrive\Desktop\MRIG\dataset_30.csv', source_column="prompt")
+    data = loader.load()
+
+    # Create a FAISS instance for vector database from 'data'
+    vectordb = FAISS.from_documents(documents=data,
+                                    embedding=instructor_embeddings)
+
+    # Save vector database locally
+    vectordb.save_local(vectordb_file_path)
+
+def get_qa_chain():
+    # Load the vector database from the local folder
+    vectordb = FAISS.load_local(vectordb_file_path, instructor_embeddings)
+
+    # Create a retriever for querying the vector database
+    retriever = vectordb.as_retriever(score_threshold=0.7)
+
+    prompt_template = """Given the following context and a question, generate an answer based on this context only.
+    In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
+    If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+
+    CONTEXT: {context}
+
+    QUESTION: {question}"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    chain = RetrievalQA.from_chain_type(llm=llm,
+                                        chain_type="stuff",
+                                        retriever=retriever,
+                                        input_key="query",
+                                        return_source_documents=False,
+                                        chain_type_kwargs={"prompt": PROMPT})
+
+    return chain
+
+def ask_question():
+    chain_instance = get_qa_chain()
+    user_query = input("Please enter your question: ")
+    response = chain_instance(user_query)
+
+    print(user_query, " : ",response['result'])
+
+if __name__ == "__main__":
+    create_vector_db()
+    ask_question()
